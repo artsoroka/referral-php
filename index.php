2df713822c9f407fdde31fc44947aa9b4727ea85
artsoroka <githubrepository@yandex.ru>
2014-11-15 01:28:41 +0400

    initial commit: simple php class to generate short url refs

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+require 'referrallink.php'; 
+
+$referral = new ReferralLink(array(
+  'url' => 'yoursite.ru',
+  'length' => 5, 
+  'keyName' => 'ref'
+));
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Referral-PHP </title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <div>Referal link: <? echo $referral->createLink(); ?>
+    </div>
+  </body>
+</html>
